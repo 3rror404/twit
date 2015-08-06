f8ea8f84a1110cd418fec3f589f9059b1c782b5b
3rror404 <daniel_rochelle@hotmail.com>
2015-08-06 22:59:33 +0100

    Update config.js
    Added twitter credentials

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    consumer_key: 'RKghjQlmS3GWT7gD2LXTFkWRo'
+  , consumer_secret: 'kSjkRGnbsprQM9X0zBwNfC0074afoh20NLeeSq5lfpdBDAWgog'
+  , access_token: '3405524205-SbtyoEPseN6KvqMhdeNa8IRz446iJA9Mse3HW4N'
+  , access_token_secret: 'uwzvlZmzlNt8D90bTukYQ6iy1UECULCc2oeGWH0mCQtF4'
+}
